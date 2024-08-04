331b0947ada0386af157c6420fe3b00caa27ef7f
oliverschuette <oliverschuette1@gmail.com>
2024-08-05 01:07:42 +0200

    fix

--- detectron2/data/transforms/transform.py
+++ detectron2/data/transforms/transform.py
@@ -139,7 +139,7 @@ class ResizeTransform(Transform):
 
                 #pil_image = Image.fromarray(img)
                 # Instead of pil-image, load in as rasterio
-                pil_image = cv2.imwrite("dummy_image.tiff" img)
+                pil_image = cv2.imwrite("dummy_image.tiff", img)
                 #with rasterio.open(img) as src:
                     #pil_image = src.read()
             pil_image = pil_image.resize((self.new_w, self.new_h), interp_method)

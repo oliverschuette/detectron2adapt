ee9dc37505d6d7b61a82b70783eaf85833139ed8
oliverschuette <oliverschuette1@gmail.com>
2024-08-06 17:07:18 +0200

    print the image size in the datasetmapper

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -154,6 +154,7 @@ class DatasetMapper:
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
         # USER: Write your own image loading if it's not from a file
         image = utils.read_image(dataset_dict["file_name"], format="TIFF")
+        print("Print our image size in the dataset_Mapper: ", image.shape)
         utils.check_image_size(dataset_dict, image)
 
         # USER: Remove if you don't do semantic/panoptic segmentation.
